00d9c324d912ce8035732d6e17a0e8036b41fcf6
Akshata Malavi <malaviakshata@gmail.com>
2025-09-10 11:53:08 +0530

    add programs files

--- tuple.py
+++ tuple.py
@@ -9,12 +9,15 @@ print(type(s)) #type = tuple
 print(s[0])  
 print(s[1:4])  #slicing
 print(s[2])
+print(s[2:4])
 
 #negative indexing of tuple
 print(s[-1])
 print(s[-3])
+print(s[-2])
 print(s[-1:-4:2])
 print(s[-1:-6:-2])
 
 print(s.count('akshata'))
 print(s.index('riya')) 
+print(s.index('sayali'))
